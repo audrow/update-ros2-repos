e9afacb0677952eda9efa0b8cf3c1625a49fc9de
Audrow Nash <audrow@openrobotics.org>
2022-11-02 12:18:37 -0500

    Add type to export
    Signed-off-by: Audrow Nash <audrow@openrobotics.org>

--- packages/ros2-cache/src/index.ts
+++ packages/ros2-cache/src/index.ts
@@ -3,7 +3,7 @@ export * as distributionFile from './distribution-file'
 export * as fileSystem from './file-system'
 export * as github from './github'
 export * as reposFile from './repos-file'
-export {DistributionFile, ReposFile}
+export type {DistributionFile, ReposFile}
 
 import DistributionFile from './__types__/DistributionFile'
 import ReposFile from './__types__/ReposFile'
