c167ce48750a24cfb7709ec4f96cb802f004a54c
Audrow Nash <audrow@openrobotics.org>
2022-11-04 11:53:17 -0500

    Update maintainers docs + script
    Signed-off-by: Audrow Nash <audrow@openrobotics.org>

--- apps/update-maintainers/src/index.ts
+++ apps/update-maintainers/src/index.ts
@@ -1,5 +1,30 @@
-export function main() {
-  //
+import {join} from 'path'
+
+export async function main() {
+  // clone repos
+  // const maintainersInfoFileName = 'maintainers-info.yaml'
+  // const dataDir = join(__dirname, '..', 'data')
+  // const reposToUpdate = 
+  const version = 'rolling'
+  const reposFileUrl = `https://raw.githubusercontent.com/ros2/ros2/${version}/ros2.repos`
+  const reposPath = join(cacheDir, `${version}.repos.yaml`)
+
+  cache.makeCacheDir({path: cacheDir, isForceRefresh: false})
+  cache.downloadFile({path: reposPath, url: reposFileUrl})
+  // const reposFileContent = reposFile.getRepos(reposPath)
+
+  const repo = repos.repositories[0]
+  const repoPath = join(cacheDir, repo.org, repo.name)
+  await cache.pullGitRepo({
+    url: repo.url,
+    version: version,
+    destinationPath: repoPath,
+  })
+
+  // update maintainers
+  // open PRs for repos
 }
 
-main()
+if (require.main === module) {
+  main()
+}
