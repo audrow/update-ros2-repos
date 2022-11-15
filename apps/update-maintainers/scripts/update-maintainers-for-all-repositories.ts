d0e24b2a9e1f763c5aaf864e763c74ddb86b5d9c
Audrow Nash <audrow@openrobotics.org>
2022-11-15 16:26:39 -0600

    Rename files
    Signed-off-by: Audrow Nash <audrow@openrobotics.org>

--- apps/update-maintainers/scripts/update-maintainers-for-all-repositories.ts
+++ apps/update-maintainers/scripts/update-maintainers-for-all-repositories.ts
@@ -57,7 +57,7 @@ export async function main() {
 
   // run it!
   const errors: {repoName: string; error: unknown}[] = []
-  for await (const repo of repositories) {
+  for await (const repo of repositories.slice(40, 60)) { // last did up to 60
     try {
       await makeUpdateMaintainersPr({
         additionalReviewers,
